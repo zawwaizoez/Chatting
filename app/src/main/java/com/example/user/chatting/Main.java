ead782908fa563ccbced25e641217e64ebcf1c92
Hsumon_Soe <hsumon.1740@gmail.com>
2017-10-10 00:13:39 +0700

    Initial commit is one

---
+++ app/src/main/java/com/example/user/chatting/Main.java
@@ -0,0 +1,20 @@
+package com.example.user.chatting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+private void test(){
+
+    int t = 1+2;
+}
+
+
+}
